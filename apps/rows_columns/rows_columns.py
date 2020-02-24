18be19380f35e43373dfb933abad3e9290dd9ae7
leon-do <leondo123@gmail.com>
2020-02-24 12:46:03 -0500

    nested rows/columns

--- apps/rows_columns/rows_columns.py
+++ apps/rows_columns/rows_columns.py
@@ -4,12 +4,14 @@ from apps.rows_columns import red_box, green_box
 
 def layout():
     return html.Div([
-        html.H4('Use className="row" and className="six columns". Max 12 columns/row'),
+        html.H4('Rows and Columns'),
 
+        html.H5('use "className"="row" to create a row'),
         html.Div([
             html.Div(red_box.layout())
         ], className='row'),
 
+        html.H5('Max 12 columns/row'),
         html.Div([
             html.Div(green_box.layout(), className='one columns'),
             html.Div(red_box.layout(), className='one columns'),
@@ -25,9 +27,19 @@ def layout():
             html.Div(green_box.layout(), className='one columns'),
         ], className='row'),
 
+        html.H5('green 9 rows, red 3 rows'),
         html.Div([
             html.Div(green_box.layout(), className='nine columns'),
             html.Div(red_box.layout(), className='three columns'),
         ], className='row'),
+
+        html.H5('nested rows and columns'),
+        html.Div([
+            html.Div([
+                html.Div(green_box.layout()),
+                html.Div(red_box.layout()),
+            ], className='row six columns'),
+            html.Div(red_box.layout(), className='six columns'),
+        ], className='row')
 
     ])
---
+++ apps/sidebar/README.md
@@ -0,0 +1,3 @@
+# Sidebar 
+
+notes: https://philipwalton.github.io/solved-by-flexbox/demos/holy-grail/
